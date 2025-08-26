1ba5b0820c6a203fb0a8c056a66f4af1e90b0f5e
Kavinda Botheju <kavinda.botheju@grubtech.com>
2025-08-26 23:28:59 +0530

    add patch to addCorsMappings

--- src/main/java/com/erp/universitycoursemanagement/config/WebCorsConfig.java
+++ src/main/java/com/erp/universitycoursemanagement/config/WebCorsConfig.java
@@ -10,7 +10,7 @@ public class WebCorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3001")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
